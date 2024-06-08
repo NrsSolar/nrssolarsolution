7c2b491c439cfc8bb8e79f31aa3563b96455aebf
PREM\DELL <premtak1991@gmail.com>
2024-06-08 18:25:21 +0530

    Update website desgin and initiate new ideas in it

---
+++ assets/js/newcustom.js
@@ -0,0 +1,46 @@
+ 
+  // Header sticky 
+  window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// banner slider
+  var currentImg = 0;
+  var imgs = document.querySelectorAll('.slider img');
+  let dots = document.querySelectorAll('.dot');
+  var interval = 3000;
+  var timer = setInterval(changeSlide, interval);
+
+  function changeSlide(n) {
+    for (var i = 0; i < imgs.length; i++) {
+      imgs[i].style.opacity = 0;
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    currentImg = (currentImg + 1) % imgs.length;
+
+    if (n != undefined) {
+      clearInterval(timer);
+      timer = setInterval(changeSlide, interval);
+      currentImg = n;
+    }
+
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+  }
+
+  function calculateDays(today, eventDate) {
+    const difference = eventDate.getTime() - today.getTime();
+
+    return Math.ceil(difference / (1000 * 3600 * 24)); // convert to days
+  }
+ 
